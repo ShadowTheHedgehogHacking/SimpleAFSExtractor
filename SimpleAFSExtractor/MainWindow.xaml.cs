71539d694b0d00c02d6ff0ab7f024206791b3637
dreamsyntax <dreamsyntax@gmail.com>
2022-04-13 14:28:50 -0700

    public release

---
+++ SimpleAFSExtractor/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using AFSLib;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace SimpleAFSExtractor {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        AfsArchive currentAfs;
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e) {
+            var dialog = new Ookii.Dialogs.Wpf.VistaOpenFileDialog
+            {
+                Filter = "AFS files (*.afs)|*.afs|All files (*.*)|*.*"
+            };
+            if (dialog.ShowDialog() == false) {
+                return;
+            }
+            if (!dialog.FileName.ToLower().EndsWith(".afs")) {
+                MessageBox.Show("Pick an 'AFS' file", "Try Again");
+                return;
+            }
+            var data = File.ReadAllBytes(dialog.FileName);
+            if (AfsArchive.TryFromFile(data, out var afsArchive)) {
+                currentAfs = afsArchive;
+                filePicked.Content = Path.GetFileName(dialog.FileName);
+                extractButton.IsEnabled = true;
+            };
+        }
+
+        private void ButtonExtract_Click(object sender, RoutedEventArgs e) {
+            if (currentAfs == null)
+                return;
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            if (dialog.ShowDialog() == false) {
+                return;
+            }
+            if (dialog.SelectedPath != "") {
+                for (int i = 0; i < currentAfs.Files.Count(); i++) {
+                    File.WriteAllBytes(dialog.SelectedPath + "\\\\" + i + "_" + currentAfs.Files[i].Name, currentAfs.Files[i].Data);
+                }
+                MessageBox.Show("Extraction completed", "Simple AFS Extractor");
+            }
+        }
+    }
+}
